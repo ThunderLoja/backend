0770e3dc39e56b9c6a220930c91fb727d2ff36e0
lucastrschneider <lucastrschneider@gmail.com>
2022-07-19 20:57:36 -0300

    🔨 Add scripts to fill tables
    Co-authored-by: Lucas Haug <LucasHaug@users.noreply.github.com>
    Co-authored-by: Antonio Seixas <AntonioSeixas1@users.noreply.github.com>

--- tables.py
+++ tables.py
@@ -1,10 +1,86 @@
 #!/usr/bin/python3
 
 import logging
-import psycopg2
-from .thunder_loja.db_handler import db_connect
+import argparse
+from thunder_loja.db_handler import DBHandler
+
+CREATE_TABLES_SCRIPT = 'thunder_loja/scripts/create_tables.sql'
+DROP_TABLES_SCRIPT = 'thunder_loja/scripts/drop_tables.sql'
+FILL_TABLES_SCRIPT = 'thunder_loja/scripts/fill_tables.sql'
+
+logger = logging.getLogger("Tables")
+
+def execute_script(db_handler: DBHandler, script: str):
+    sql_file = None
+
+    try:
+        with open(script, 'r') as file:
+            sql_file = file.read()
+    except FileNotFoundError:
+        logger.error("File not found")
+        return False
+
+    # Remove new lines, split commands and remove empty commands
+    sql_commands = sql_file.replace('\n','').split(';')
+    sql_commands = [i for i in sql_commands if i]
+
+    conn = db_handler.connect()
+    
+    if conn is not None:
+        cur = conn.cursor()
+
+        # Execute every command from the input file
+        for command in sql_commands:
+            # This will skip and report errors
+            # For example, if the tables do not yet exist, this will skip over
+            # the DROP TABLE commands
+            try:
+                cur.execute(command)
+            except Exception as e:
+                logger.error(f"Command skipped: {e}")
+        
+        conn.commit()
+        cur.close()
+        conn.close()
+        return True
+
+    return False
 
 
-def create_tables():
-    pass
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", metavar="A", type=str,
+                        help="Action to perform ('create', 'drop', 'fill')")
 
+    args = parser.parse_args()
+
+    return args.action
+
+
+def main():
+    action = parse_arguments()
+
+    db_handler = DBHandler(config_file="cfg/database.ini",
+                           config_section="thunder_loja_db")
+
+    script_file = None
+
+    if action == 'create':
+        logger.info('Creating tables')
+        script_file = CREATE_TABLES_SCRIPT
+    elif action == 'drop':
+        logger.info('Dropping tables')
+        script_file = DROP_TABLES_SCRIPT
+    elif action == 'fill':
+        logger.info('Filling tables')
+        script_file = FILL_TABLES_SCRIPT
+    else:
+        logger.error('Argument not suported')
+    
+    if script_file is not None:
+        execute_script(db_handler, script_file)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
---
+++ thunder_loja/scripts/fill_tables.sql
@@ -0,0 +1,35 @@
+INSERT INTO  colaborador(colab_id, colab_nome, colab_cpf, colab_salario, colab_data_admi, colab_tipo, colab_ger_id, colab_senha)
+ VALUES(00000000001, 'Antonio', 00000000001, 1000.00, '2000-08-01', 1, 00000000001, 'senha1');
+
+INSERT INTO  colaborador(colab_id, colab_nome, colab_cpf, colab_salario, colab_data_admi, colab_tipo, colab_ger_id, colab_senha)
+ VALUES(00000000002, 'Lucas Guedes', 00000000002, 200.00, '2000-08-02', 2, 00000000002, 'senha2');
+
+INSERT INTO  colaborador(colab_id, colab_nome, colab_cpf, colab_salario, colab_data_admi, colab_tipo, colab_ger_id, colab_senha)
+ VALUES(00000000003, 'Greghory', 00000000003, 300.00, '2000-08-03', 2, 00000000003, 'senha3');
+
+INSERT INTO  colaborador(colab_id, colab_nome, colab_cpf, colab_salario, colab_data_admi, colab_tipo, colab_ger_id, colab_senha)
+ VALUES(00000000004, 'Vanderson', 00000000004, 400.00, '2000-08-04', 2, 00000000004, 'senha4');
+
+INSERT INTO  colaborador(colab_id, colab_nome, colab_cpf, colab_salario, colab_data_admi, colab_tipo, colab_ger_id, colab_senha)
+ VALUES(00000000005, 'Lucas Schneider', 00000000005, 500.00, '2000-08-05', 2, 00000000005, 'senha5');
+
+INSERT INTO produto(prod_id, prod_nome, prod_valor, prod_descricao, prod_categoria)
+ VALUES(00000000001, 'Galena', 5000.00, 'robo foda, pesadelo da mauah', 'robo pesado');
+
+INSERT INTO produto(prod_id, prod_nome, prod_valor, prod_descricao, prod_categoria)
+ VALUES(00000000002, 'Moai', 5000.00, 'robo relampago marquinhos maromba', 'robo rapidaum');
+
+INSERT INTO estoque(prod_id, loja_id, est_quant)
+ VALUES(00000000001, 1, 1);
+
+INSERT INTO estoque(prod_id, loja_id, est_quant)
+ VALUES(00000000002, 1, 1);
+
+INSERT INTO cliente(clt_cpf, clt_nome)
+ VALUES(00000000006, 'Haug');
+
+INSERT INTO cliente(clt_cpf, clt_nome)
+ VALUES(00000000007, 'Renzo');
+
+INSERT INTO transacao(tr_id, tr_valor, tr_data, tr_descricao, tr_tipo)
+ VALUES(00000000001, 400.00, '2000-04-01', 'compra do maxon', '1');
