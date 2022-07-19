b44d6adbf1235dcb0bc34eb7a42df799366bbd54
lucastrschneider <lucastrschneider@gmail.com>
2022-07-19 18:02:59 -0300

    🐛 Fix bug in db handler method
    Co-authored-by: Lucas Haug <LucasHaug@users.noreply.github.com>
    Co-authored-by: Antonio Seixas <AntonioSeixas1@users.noreply.github.com>

--- thunder_loja/db_handler.py
+++ thunder_loja/db_handler.py
@@ -25,7 +25,7 @@ class DBHandler():
             for param in params:
                 db[param[0]] = param[1]
         else:
-            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+            raise Exception('Section {0} not found in the {1} file'.format(self._config_section, self._config_file))
 
         return db
 
