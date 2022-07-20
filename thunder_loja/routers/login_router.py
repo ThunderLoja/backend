4d68c5ce3405d27cff5cc0a6c86c6c18dabcec74
lucastrschneider <lucastrschneider@gmail.com>
2022-07-20 01:10:20 -0300

    🥅  Update login router error handling
    Co-authored-by: Lucas Haug <LucasHaug@users.noreply.github.com>
    Co-authored-by: Antonio Seixas <AntonioSeixas1@users.noreply.github.com>

--- thunder_loja/routers/login_router.py
+++ thunder_loja/routers/login_router.py
@@ -22,6 +22,7 @@ router = APIRouter(
 # Logger
 logger = logging.getLogger('LoginRoute')
 
+
 @router.put("/")
 async def get(colab_data: ColabLoginData):
     sql = f"""
@@ -32,20 +33,23 @@ async def get(colab_data: ColabLoginData):
     db_handler = DBHandler()
     colaborator = db_handler.send_command(sql)
 
-    if colaborator is not None and colaborator:
-        logger.debug(colaborator)
+    if colaborator is not None:
+        if colaborator:
+            logger.debug(colaborator)
 
-        colab_data = ColabData(
-            id=colaborator[0][0],
-            name=colaborator[0][1],
-            cpf=colaborator[0][2],
-            salary=colaborator[0][3],
-            admission_date=colaborator[0][4],
-            colab_type=colaborator[0][5],
-            is_active=colaborator[0][6],
-            manager_id=colaborator[0][7]
-        )
+            colab_data = ColabData(
+                id=colaborator[0][0],
+                name=colaborator[0][1],
+                cpf=colaborator[0][2],
+                salary=colaborator[0][3],
+                admission_date=colaborator[0][4],
+                colab_type=colaborator[0][5],
+                is_active=colaborator[0][6],
+                manager_id=colaborator[0][7]
+            )
 
-        return colab_data
+            return colab_data
+        else:
+            raise HTTPException(status_code=401, detail="Login failed")
     else:
-        raise HTTPException(status_code=401, detail="Login failed")
+        raise HTTPException(status_code=500, detail="Failed to get contributors")
