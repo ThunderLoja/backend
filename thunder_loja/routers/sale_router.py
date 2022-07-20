8596bea737516c3c1315ed18fbce904bb8da37e2
lucastrschneider <lucastrschneider@gmail.com>
2022-07-20 11:52:59 -0300

    ✨ Add sale report route

--- thunder_loja/routers/sale_router.py
+++ thunder_loja/routers/sale_router.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Union
+import datetime
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
@@ -7,11 +7,12 @@ from thunder_loja.db_handler import DBHandler
 
 
 # Data
-class SaleData(BaseModel):
-    transaction_id: int
-    client_cpf: int
-    colab_id: int
-
+class SaleReportData(BaseModel):
+    id: int
+    name: str
+    price: float
+    quantity_available: int
+    quantity_sold: int
 
 # Router
 router = APIRouter(
@@ -26,13 +27,59 @@ logger = logging.getLogger('SaleRoute')
 
 
 @router.get("/relatorio")
-async def get(q: Union[str, None] = None):
+async def get(start: datetime.date = None, end: datetime.date = None):
     # Join 
-    return {"message": f"Hello Relatorio de Venda"}
+    logger.debug(f"Parameters: {start} {end}")
+
+    where_string = ""
+    if start is not None:
+        if end is not None:
+            if start > end:
+                raise HTTPException(status_code=400, detail='Start date greater then end date')
+            where_string = f"WHERE tr_data between '{start}' and '{end}'"
+        
+        else:
+            where_string = f"WHERE tr_data >= '{start}'"
+    else:
+        if end is not None:
+            where_string = f"WHERE tr_data <= '{end}'"
+
+    sql = f"""
+           SELECT prod_id, prod_nome, prod_valor, prod_quant, sum(vend_quant)
+           FROM produto
+           NATURAL JOIN prod_venda
+           NATURAL JOIN transacao
+           {where_string}
+           GROUP BY prod_id, prod_nome, prod_valor, prod_quant
+           ORDER BY prod_id
+           """
+    db_handler = DBHandler()
+
+    products, error_msg = db_handler.send_command(sql)
+
+    if error_msg is None:
+        report_data = []
+        
+        for product in products:
+            report_data.append(
+                SaleReportData(
+                    id=product[0],
+                    name=product[1],
+                    price=product[2],
+                    quantity_available=product[3],
+                    quantity_sold=product[4]
+                )
+            )
+            logger.debug(f"Product report: {product}")
+        
+        return report_data
+    else:
+        logger.error(error_msg)
+        raise HTTPException(status_code=500, detail=error_msg)
 
 
 @router.post("/nova")
-async def post(sale_data: SaleData):
+async def post(sale_data: SaleReportData):
     # Dados do json: transação, venda, [(prod_id, quant)...]
     # Criar nova linha em transacao
     # Criar nova linha em venda
