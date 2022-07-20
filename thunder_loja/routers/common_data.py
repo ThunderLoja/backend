dbdfa2448b38699cf74e43a297bac0011141f218
lucastrschneider <lucastrschneider@gmail.com>
2022-07-20 01:00:03 -0300

    ✨ Finish login route
    Co-authored-by: Lucas Haug <LucasHaug@users.noreply.github.com>
    Co-authored-by: Antonio Seixas <AntonioSeixas1@users.noreply.github.com>

---
+++ thunder_loja/routers/common_data.py
@@ -0,0 +1,22 @@
+import datetime
+from pydantic import BaseModel
+
+
+class ColabData(BaseModel):
+    id: int
+    name: str
+    cpf: int
+    salary: float
+    admission_date: datetime.date
+    colab_type: int
+    is_active: bool
+    manager_id: int
+
+
+class ProductData(BaseModel):
+    id: int
+    name: str
+    price: float
+    description: str
+    category: str
+    quantity: int
--- thunder_loja/routers/login_router.py
+++ thunder_loja/routers/login_router.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+import logging
+
+from thunder_loja.db_handler import DBHandler
+from thunder_loja.routers.common_data import ColabData
 
 
 # Data
-class ColabData(BaseModel):
+class ColabLoginData(BaseModel):
     id: int
     password: str
 
@@ -15,10 +19,33 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+# Logger
+logger = logging.getLogger('LoginRoute')
 
-@router.get("/")
-async def get(colab_data: ColabData):
-    if colab_data.id == 1 and colab_data.password == "42":
-        return {"message": "Login successful"}
+@router.put("/")
+async def get(colab_data: ColabLoginData):
+    sql = f"""
+           SELECT colab_id, colab_nome, colab_cpf, colab_salario, colab_data_admi, colab_tipo, colab_ativo, colab_ger_id
+           FROM colaborador
+           WHERE colab_id = {colab_data.id} and colab_senha = '{colab_data.password}' and colab_ativo = B'1'
+           """
+    db_handler = DBHandler()
+    colaborator = db_handler.send_command(sql)
+
+    if colaborator is not None and colaborator:
+        logger.debug(colaborator)
+
+        colab_data = ColabData(
+            id=colaborator[0][0],
+            name=colaborator[0][1],
+            cpf=colaborator[0][2],
+            salary=colaborator[0][3],
+            admission_date=colaborator[0][4],
+            colab_type=colaborator[0][5],
+            is_active=colaborator[0][6],
+            manager_id=colaborator[0][7]
+        )
+
+        return colab_data
     else:
         raise HTTPException(status_code=401, detail="Login failed")
